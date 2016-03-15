7a51c5f7fedacc8ecb8ebdef9d9f6abe699b3368
ckennard <chriskennard123@gmail.com>
2016-03-14 22:15:39 -0700

    Added message header RNC

--- interface/decode.c
+++ interface/decode.c
@@ -29,6 +29,22 @@ struct arguments {
     char * chunks[3];
 };
 
+#define TYPE_POST_CHUNK 1
+//post chunk followed by ChunkHeader and payload
+
+#define TYPE_REQUEST_CHUNK 2
+//request chunk followed by null-terminated chunk file name str
+
+#define TYPE_ACKNOWLEDGE 3
+//message followed by nothing
+
+#define TYPE_ERROR 4
+//message followed by null-terminated error string
+
+struct MessageHeader {
+  int Type;
+  int MessageLen; //the length of the data that follows this header
+}
 //
 //
 //
